9bf73abf5e7714b86fc45bd1dc2f13a5a5547f27
EC2 Default User <ec2-user@ip-172-31-8-143.ap-northeast-1.compute.internal>
2025-03-15 01:44:19 +0000

    [Add]controllers

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,20 @@
+class Public::OrdersController < ApplicationController
+  def new
+  end
+ 
+  def confirm
+  end
+
+  def finish
+  end
+ 
+  def create
+  end
+ 
+  def index
+  end
+ 
+  def show
+  end
+ 
+end
