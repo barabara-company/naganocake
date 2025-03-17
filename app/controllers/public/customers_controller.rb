0ed8e9bd99ce6dc627444eb48d5f3377aeab402f
EC2 Default User <ec2-user@ip-172-31-8-143.ap-northeast-1.compute.internal>
2025-03-17 09:05:29 +0000

    [Finish] public-customers

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,16 +1,26 @@
 class Public::CustomersController < ApplicationController
+
+  before_action :authenticate_user!  #ログインしてなかったらログイン画面へ移行
+
   def show
+    @customer = current_customer
   end
     
   def edit
-  end    
+    @customer = current_customer
+  end 
 
   def update
+    current_customer.update
+    redirect_to customer_path(current_customer)
   end
     
   def unsubscribe
   end
     
   def withdraw
-  end    
+    current_customer.update(is_active: false) #会員ステータスをfalseに変更
+    reset_session #ログアウト処理
+    redirect_to root_path
+  end
 end
