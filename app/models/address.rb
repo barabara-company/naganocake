7cb7ef56c694dfb1f290e2e348e889f0100ce23e
EC2 Default User <ec2-user@ip-172-31-5-203.ap-northeast-1.compute.internal>
2025-03-18 06:08:49 +0000

    [add]address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,8 @@
 class Address < ApplicationRecord
+  belongs_to :customer
+
+  validates :postal_code, presence: true, format: { with: /\A\d{7}\z/, message: 'は7桁の数字で入力してください' }
+  validates :address, presence: true
+  validates :name, presence: true
+
 end
