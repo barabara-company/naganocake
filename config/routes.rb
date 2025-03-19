26d882f8073149b961a32f0d101b4827afde213f
EC2 Default User <ec2-user@ip-172-31-5-203.ap-northeast-1.compute.internal>
2025-03-19 08:58:31 +0000

    [finish]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
 
-  devise_for :admins
-  devise_for :customers, controllers: { sessions: 'public/devise/sessions' }
+# 顧客用
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
 
   # 会員側のルーティング
   scope module: :public do
