f2ac64b2819d4580898ea89d6bc5cd7ac9467fe7
EC2 Default User <ec2-user@ip-172-31-5-203.ap-northeast-1.compute.internal>
2025-03-20 05:58:45 +0000

    [finish]top

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,13 @@
 class Order < ApplicationRecord
+  belongs_to :customer
+  has_many :order_details, dependent: :destroy
+
+  enum status: { 
+    waiting_for_payment: 0, 
+    payment_confirmed: 1, 
+    in_production: 2, 
+    preparing_for_shipment: 3, 
+    shipped: 4 
+  }
+
 end
