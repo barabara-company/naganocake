eb5aede624a382ce6d2ef9797bd4bfe5fa78b1c0
EC2 Default User <ec2-user@ip-172-31-5-203.ap-northeast-1.compute.internal>
2025-03-21 09:07:45 +0000

    計算方法修正

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,7 +1,7 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order, foreign_key: "orders_id"
 
-  belongs_to ::item 
+  belongs_to :item 
 
   enum production_status: { not_possible: 0, pending: 1, in_production: 2, completed: 3 }
 
