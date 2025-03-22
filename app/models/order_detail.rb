51b08f1a1f8203bbb8a999e84c268c44e2ee9c2e
EC2 Default User <ec2-user@ip-172-31-5-203.ap-northeast-1.compute.internal>
2025-03-22 01:58:45 +0000

    [Add]order details

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,15 +1,11 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order, foreign_key: "orders_id"
-
-
-  
-
+  belongs_to :item, foreign_key: "item_id"
   enum production_status: { not_possible: 0, pending: 1, in_production: 2, completed: 3 }
 
   def subtotal
     unit_price * quantity
   end
 
-  belongs_to :item, foreign_key: "item_id"
-
 end
+
