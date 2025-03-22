486e16fe23cfdbada7bca7d1df409c0ed8da040b
EC2 Default User <ec2-user@ip-172-31-8-143.ap-northeast-1.compute.internal>
2025-03-22 04:54:41 +0000

    [Finish] adominアカウントのseed作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+# rails db:seed で実行
+
+# サンプルadminアカウントの作成
+# find_or_create_by ですでに同じメールアドレスの存在を確認 存在すれば作らない。
+# do |admin| で存在しなければ他の属性も追加で新規作成する
+
+admin = Admin.find_or_create_by(email: "admin@example.com") do |admin|
+  admin.password = "123456"
+  puts "email = admin@example.com"
+  puts "password = 123456"
+end
