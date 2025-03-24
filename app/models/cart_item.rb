a39bb0f240b88851c025670032177ce1d5dbde35
EC2 Default User <ec2-user@ip-172-31-8-143.ap-northeast-1.compute.internal>
2025-03-24 00:16:20 +0000

    [Add] model validate追加

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,9 @@
 class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
+
+  validates :item, presence: true
+  validates :customer, presence: true
+  validates :amount, presence: true
+
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -4,8 +4,19 @@ class OrderDetail < ApplicationRecord
   
   enum making_status: { not_possible: 0, pending: 1, in_production: 2, completed: 3 }
 
+  # hogehoge_id	DBレベルに近く、単純で高速	存在しないIDでもバリデ通る
+  #	hogehoge 関連先が存在するかも確認できる	少しだけ遅い（無視できるレベル）関連先が存在しないと困るならこっち
+  validates :item, presence: true
+  validates :order, presence: true
+
+  validates :price, presence: true
+  validates :amount, presence: true
+  validates :making_status, presence: true
+
   def subtotal
     price * amount
   end
+
+
 end
 
